c98db3eb6638fb19f679aa3e2e1e94451a1c5bc5
Boston Dell-Vandenberg <boston@pomelodesign.com>
2015-11-25 08:44:03 -0500

    Formatting, prefix id with 'google:'

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var express = require('express')
-  , request = require('request')
-  , cors = require('cors')
-  , FirebaseTokenGenerator = require("firebase-token-generator")
-  , app = express();
+var express = require('express');
+var request = require('request');
+var cors = require('cors');
+var FirebaseTokenGenerator = require('firebase-token-generator');
+var app = express();
 
 module.exports = function(config) {
   var tokenGenerator = new FirebaseTokenGenerator(config.firebase_secrect);
@@ -10,17 +10,15 @@ module.exports = function(config) {
   app.use(cors());
    
   app.get('/', function(req, res, next){
-    console.log(req.query.id_token);
     var token = req.query.id_token;
     request('https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=' + token, function (error, response, body) {
-      console.log(body);
       if (!error && response.statusCode == 200) {
         //valid token
         var tokenInfo = JSON.parse(body);
         //check whether aud contains clientid and hd, if provided, matches configured hd
-        if(tokenInfo.aud.indexOf(config.client_id) > -1 && ( !config.hd || tokenInfo.hd === config.hd ) ) {
+        if (tokenInfo.aud.indexOf(config.client_id) > -1 && (!config.hd || tokenInfo.hd === config.hd)) {
           var firebaseTokenInfo = {
-            uid: tokenInfo.sub,
+            uid: 'google:' + tokenInfo.sub,
             email: tokenInfo.email,
             given_name: tokenInfo.given_name,
             family_name: tokenInfo.family_name
@@ -37,4 +35,4 @@ module.exports = function(config) {
   });
 
   return app;
-}
+};
